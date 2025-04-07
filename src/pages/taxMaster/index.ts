5d8f3888b6bfe130543dcc9eede4bfe72606259c
Akshay Kumar <akshay.putta3@gmail.com>
2025-04-07 07:57:31 +0530

    refactor(tax): remove TaxDetails component and related exports

--- src/pages/taxMaster/index.ts
+++
@@ -1,8 +0,0 @@
-import TaxList from "./TaxList";
-import TaxForm from "./TaxForm";
-import TaxDetails from "./TaxDetails";
-
-export { TaxList, TaxForm, TaxDetails };
-
-// Default export for lazy loading
-export default TaxList;
